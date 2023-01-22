9776d0c8701495f5a561ca439fa48b21c9a71d3f
Heejun-0219 <heejun0219@gmail.com>
2023-01-23 03:09:08 +0900

    push_swap

--- push_swap/push_swap_src/move_utilts.c
+++ push_swap/push_swap_src/move_utilts.c
@@ -9,5 +9,17 @@ int find_distance(int index, int size)
 
 int find_sector(t_list *a, int a_size, int content)
 {
-    
+    const t_list *last = ft_lstlast(a);
+    int i;
+
+    //최댓값 최솟값 찾기
+    if (content < a->content && content > last->content)
+        return (0);
+    i = 0;
+    while (!(content > a->content && content < (a->next)->content))
+    {
+        a = a->next;
+        i++;
+    }
+    return (find_distance(i, a_size));    
 }
