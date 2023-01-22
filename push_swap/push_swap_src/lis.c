64b21017607441cbd9722785c9c0b0d53f025fbf
Heejun-0219 <heejun0219@gmail.com>
2023-01-22 21:26:58 +0900

    push_swap

--- push_swap/push_swap_src/lis.c
+++ push_swap/push_swap_src/lis.c
@@ -2,5 +2,35 @@
 
 int *count_lis(t_list *a, int size, int *max)
 {
-    
+    int *step;
+    int i;
+    int j;
+    t_list *tmp;
+
+    step = (int *)malloc(sizeof(int) * size);
+    if (!step)
+        exit(-1);
+    i = 0;
+    while (i < size)
+        step[i++] = 1;
+    i = 0;
+    while (i < size)
+    {
+        j = i;
+        tmp = a;
+        while (j < size)
+        {
+            if (a->content < tmp->content && step[i] == step[j])
+            {
+                step[j]++;
+                if (step[j] > *max)
+                    *max = step[j];
+            }
+            tmp = tmp->next;
+            j++;
+        }
+        a = a->next;
+        i++;
+    }
+    return ();   
 }
