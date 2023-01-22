ea0306b9200628bd8841c59877457c9ae7514e7c
Heejun-0219 <heejun0219@gmail.com>
2023-01-23 02:06:38 +0900

    push_swap

--- push_swap/push_swap_src/move_to.c
+++ push_swap/push_swap_src/move_to.c
@@ -30,15 +30,15 @@ int find_pos(t_list *a, int *lis, int max)
 void    move_to_b(t_list **a, t_list **b, int *lis, int max)
 {
     int a_size;
-    int count_ra;
+    int count_mov;
 
     a_size = ft_lstsize(*a);
     while (a_size > max)
     {
-        count_ra = find_pos(*a, lis, max);
-        if (count_ra == -1)
+        count_mov = find_pos(*a, lis, max);
+        if (count_mov == -1)
             eexit();
-        while (count_ra-- > 0)
+        while (count_mov-- > 0)
             ra(a);
         pb(a, b);
         a_size = ft_lstsize(*a);
